df80267a018b90fb60f58d789dc57fbcc817696e
hsxonz <heroallstart2001@gmail.com>
2022-12-06 04:03:11 +0700

    Add project files.

---
+++ PuppeteerSharpTest/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace PuppeteerSharpTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCapture = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCapture
+            // 
+            this.btnCapture.Location = new System.Drawing.Point(715, 165);
+            this.btnCapture.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnCapture.Name = "btnCapture";
+            this.btnCapture.Size = new System.Drawing.Size(208, 48);
+            this.btnCapture.TabIndex = 0;
+            this.btnCapture.Text = "Run profile id";
+            this.btnCapture.UseVisualStyleBackColor = true;
+            this.btnCapture.Click += new System.EventHandler(this.btnCapture_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(100, 54);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(515, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(715, 54);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 35);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "get ProfileId";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(100, 178);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(515, 22);
+            this.textBox2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1229, 554);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnCapture);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCapture;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ PuppeteerSharpTest/Form1.cs
@@ -0,0 +1,112 @@
+﻿using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using PuppeteerSharp;
+using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PuppeteerSharpTest
+{
+    public partial class Form1 : Form
+    {
+        public String GetMethod(String url)
+        {
+            var json = "";
+
+            using (var client = new HttpClient())
+            {
+                var endpoint = new Uri(url);
+                var result = client.GetAsync(endpoint).Result;
+                json = result.Content.ReadAsStringAsync().Result;
+            }
+            return json;
+        }
+
+        private String openprofile(String id)
+        {
+            var mo = GetMethod($"http://localhost:55555/profiles/{id}/start-automation");
+            var jsonResult = (JObject)JsonConvert.DeserializeObject(mo);
+            return jsonResult["data"]["wsEndpoint"].ToString(); 
+
+
+        }
+
+        private String getRandomProfileId()
+        {
+            var mo = GetMethod($"http://localhost:55555/profiles");
+            var jsonResult = (JObject)JsonConvert.DeserializeObject(mo);
+            var profileIds = jsonResult["data"]["lst_profile"];
+            foreach (var profileId in profileIds)
+            {
+                return profileId["id"].ToString();
+            }
+            return "";
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnCapture_Click(object sender, EventArgs e)
+        {
+
+            var wsEnpoint = openprofile(textBox1.Text);
+            textBox2.Text = wsEnpoint;
+            var options = new ConnectOptions()
+            {
+                BrowserWSEndpoint = wsEnpoint
+            };
+
+            var url = "https://www.google.com/";
+
+            var browser = await PuppeteerSharp.Puppeteer.ConnectAsync(options);
+
+            // Create a new page and go to Bing Maps
+            Page page = null;
+
+            page = browser.NewPageAsync().Result;
+            await page.GoToAsync("https://google.com");
+
+
+            await page.WaitForSelectorAsync("[name='q']");
+            await page.FocusAsync("[name='q']");
+            await page.Keyboard.TypeAsync("Hello wworld");
+            await page.Keyboard.PressAsync("Enter");
+           // await page.ClickAsync(".searchbox-searchbutton");
+           // await page.WaitForNavigationAsync();
+
+           // string content = await page.GetContentAsync();
+           // richTextBox1.Text = content;
+
+
+           // await page.SetViewportAsync(new ViewPortOptions
+           // {
+           //     Width = 1125,
+           //     Height = 2436
+           // });
+           // Thread.Sleep(3000);
+           // await page.ScreenshotAsync("E:\\ai\\screenshot.png");
+
+           // Close the browser
+           //await browser.CloseAsync();
+           // pictureBox1.Image = Image.FromFile("E:\\ai\\screenshot.png");
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var id = getRandomProfileId();
+            textBox1.Text = id;
+        }
+    }
+}
